bbe4549a5215f9184e291a26e894df8fd26f0823
francisco aulet <evascroll@hotmail.com>
2023-05-13 12:48:16 -0400

    Created board

---
+++ tic_tac_toe.py
@@ -0,0 +1,13 @@
+#board layout#
+board = [1,2,3,4,5,6,7,8,9]
+
+def display_board():
+    print("-------------")
+    print("| " + board[0] + " | " + board[1] + " | " + board[2] + " |")
+    print("-------------")
+    print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |")
+    print("-------------")
+    print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |")
+    print("-------------")
+
+
